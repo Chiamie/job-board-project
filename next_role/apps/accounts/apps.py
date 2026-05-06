9dc29a7aecf79389d1fb4dea3e59f36ac4ba96c8
Chiamie <okechukwurchiamaka@gmail.com>
2026-05-06 09:55:36 +0100

    extended User model using One-to-One relationship;  Multiple resumes per candidate; Profiles created automatically after User is created

--- next_role/apps/accounts/apps.py
+++ next_role/apps/accounts/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'apps.accounts'
+
+    def ready(self):
+        import apps.accounts.sinals
+
+
--- next_role/apps/accounts/models.py
+++ next_role/apps/accounts/models.py
@@ -1,5 +1,9 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
 
 
 class User(AbstractUser):
@@ -25,3 +29,52 @@ class User(AbstractUser):
 
     def is_candidate(self):
         return self.role == self.Role.CANDIDATE
+
+
+
+
+
+class CandidateProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="candidate_profile"
+    )
+
+    bio = models.TextField(blank=True)
+    experience_years = models.PositiveIntegerField(default=0)
+    location = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"CandidateProfile({self.user})"
+
+class Resume(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    candidate = models.ForeignKey(
+        CandidateProfile,
+        on_delete=models.CASCADE,
+        related_name="resumes"
+    )
+
+    title = models.CharField(max_length=255, blank=True)
+    is_default = models.BooleanField(default=False)
+    file = models.FileField(upload_to="resumes/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Resume({self.candidate})"
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="employer_profile"
+    )
+
+    company_name = models.CharField(max_length=255)
+    company_description = models.TextField(blank=True)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Profile for {self.company_name}"
