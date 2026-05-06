325cf274618af05f59c3575c450a347693665f16
Chiamie <okechukwurchiamaka@gmail.com>
2026-05-06 05:30:04 +0100

    the accounts app has the User model and has been registered in the admin

--- next_role/apps/accounts/admin.py
+++ next_role/apps/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("id", "email", "role", "is_staff")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role Info", {"fields": ("role",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Role Info", {"fields": ("role",)}),
+    )
--- next_role/apps/accounts/models.py
+++ next_role/apps/accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        CANDIDATE = "candidate", "Candidate"
+        EMPLOYER = "employer", "Employer"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.CANDIDATE
+    )
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    phone_number = models.CharField(max_length=11, null=False, blank=False)
+    password = models.CharField(max_length=128, null=False, blank=False)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def is_employer(self):
+        return self.role == self.Role.EMPLOYER
+
+    def is_candidate(self):
+        return self.role == self.Role.CANDIDATE
